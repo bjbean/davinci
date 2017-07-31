c594a4792630ed5ee529000b68369ccde079a2e4
SunSen <Sensun@creditease.cn>
2017-07-31 11:01:42 +0800

    md5

--- server/src/main/scala/edp/davinci/util/MD5Utils.java
+++ server/src/main/scala/edp/davinci/util/MD5Utils.java
@@ -1,17 +1,30 @@
 package edp.davinci.util;
-import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
-import java.security.*;
+
+import java.security.MessageDigest;
 
 public class MD5Utils {
-    public static String getMD5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        // 生成一个MD5加密计算摘要
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        // 计算md5函数
-        md.update(str.getBytes("UTF-8"));
-        // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
-        // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-        return new BigInteger(1, md.digest()).toString(16);
-    }
+    public static String getMD5(String str) throws Exception{
+        try {
+            MessageDigest instance = MessageDigest.getInstance("MD5");// 获取MD5算法对象
+            byte[] digest = instance.digest(str.getBytes());// 对字符串加密,返回字节数组
 
+            StringBuffer sb = new StringBuffer();
+            for (byte b : digest) {
+                int i = b & 0xff;// 获取字节的低八位有效值
+                String hexString = Integer.toHexString(i);// 将整数转为16进制
+                // System.out.println(hexString);
+
+                if (hexString.length() < 2) {
+                    hexString = "0" + hexString;// 如果是1位的话,补0
+                }
+
+                sb.append(hexString);
+            }
+
+            return sb.toString();
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 }
